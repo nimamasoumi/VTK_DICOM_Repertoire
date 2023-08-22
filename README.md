dfbf81bc05ab017f418b2d46bd38b24b8aa60c79
nimamasoumi <nimamasoumi1@gmail.com>
2023-08-22 18:32:57 -0400

    Initializing VTK DICOM

---
+++ README.md
@@ -0,0 +1,6 @@
+# VTK DICOM Repertoire using C++
+Programming conceptual C++ codes using VTK libraries for lernears and enthusiasts. 
+The focus is going to be on reading, writing, and processing DICOM data. 
+
+![alt-text](https://github.com/nimamasoumi/VTK_DICOM_Repertoire/blob/main/dcm.png?raw=true)
+
---
+++ dicomProcess.cxx
@@ -0,0 +1,42 @@
+#include <vtkDICOMImageReader.h>
+#include <vtkImageViewer2.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+
+int main(int argc, char* argv[])
+{
+    vtkNew<vtkNamedColors> colors;
+
+    // Verify input arguments
+    if (argc != 2)
+    {
+        std::cout << "Please specify the DICOM file."<<std::endl;
+        std::cout << "Usage: " << argv[0] << " <Filename>"<< std::endl;
+        return EXIT_FAILURE;
+    }
+    std::string inputFilename = argv[1];
+
+    // Read all the DICOM files in the specified directory.
+    vtkNew<vtkDICOMImageReader> reader;
+    reader->SetFileName(inputFilename.c_str());
+    reader->Update();
+
+    // Visualize
+    vtkNew<vtkImageViewer2> imageViewer;
+    imageViewer->SetInputConnection(reader->GetOutputPort());
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    imageViewer->SetupInteractor(renderWindowInteractor);
+    imageViewer->Render();
+    imageViewer->GetRenderer()->SetBackground(
+        colors->GetColor3d("SlateGray").GetData());
+    imageViewer->GetRenderWindow()->SetWindowName("VTK DICOM");
+    imageViewer->GetRenderer()->ResetCamera();
+    imageViewer->Render();
+
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
+}
